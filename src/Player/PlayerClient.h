64f753732a90b2c7d49784f3ebca10756f34a6ec
M4xB3k <147445017+M4xB3k@users.noreply.github.com>
2026-01-13 00:45:03 +0100

    Feature: Add TankSpriteManager with health-based damaged sprites

--- src/Player/PlayerClient.h
+++ src/Player/PlayerClient.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "PlayerState.h"
+#include "TankSpriteManager.h"
 #include "Map/MapState.h"
 #include <SFML/Graphics.hpp>
 #include <functional>
@@ -47,10 +48,9 @@ class PlayerClient
 	// visuals
 	sf::Color m_color;
 
-	std::array<sf::Texture *, 8> m_hullTextures;
-	std::array<sf::Texture *, 8> m_turretTextures;
 	sf::Sprite m_hullSprite;
 	sf::Sprite m_turretSprite;
+	TankSpriteManager::TankState m_currentTankState;
 
 	sf::Font &m_font; // must be declared before m_nameText
 	sf::Text m_nameText;
---
+++ src/Player/TankSpriteManager.h
@@ -0,0 +1,71 @@
+#pragma once
+#include "../ResourceManager.h"
+#include "../SpriteSheet.h"
+#include <memory>
+
+class TankSpriteManager
+{
+  public:
+	enum class TankState
+	{
+		HEALTHY,
+		DAMAGED
+	};
+
+	enum class TankPart
+	{
+		HULL,
+		TURRET
+	};
+
+	TankSpriteManager(TankSpriteManager const &) = delete;
+	TankSpriteManager &operator=(TankSpriteManager const &) = delete;
+
+	static TankSpriteManager &inst()
+	{
+		static TankSpriteManager s_instance;
+		return s_instance;
+	}
+
+	[[nodiscard]] sf::Sprite getSprite(TankPart part, TankState state, int direction) const
+	{
+		int index = getSpriteIndex(part, state, direction);
+		return m_spriteSheet->getSprite(index);
+	}
+
+	[[nodiscard]] SpriteSheet const &getSpriteSheet() const
+	{
+		return *m_spriteSheet;
+	}
+
+  private:
+	TankSpriteManager()
+	{
+		sf::Texture &texture = TextureManager::inst().load("tanks/tiger/hull_spritesheet.png");
+		m_spriteSheet = std::make_unique<SpriteSheet>(texture, SPRITE_SIZE, SPRITE_SIZE, COLUMNS);
+	}
+
+	[[nodiscard]] int getSpriteIndex(TankPart part, TankState state, int direction) const
+	{
+		int row = 0;
+
+		if(part == TankPart::HULL)
+		{
+			row = (state == TankState::HEALTHY) ? HEALTHY_HULL_ROW : DAMAGED_HULL_ROW;
+		}
+		else
+		{
+			row = TURRET_ROW;
+		}
+
+		return row * COLUMNS + direction;
+	}
+
+	std::unique_ptr<SpriteSheet> m_spriteSheet;
+
+	static constexpr int COLUMNS = 8;
+	static constexpr int SPRITE_SIZE = 150;
+	static constexpr int HEALTHY_HULL_ROW = 0;
+	static constexpr int DAMAGED_HULL_ROW = 1;
+	static constexpr int TURRET_ROW = 2;
+};
