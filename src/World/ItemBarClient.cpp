f381b37dfbe633cdd710b1c51e845f382fdb6b17
M4xB3k <147445017+M4xB3k@users.noreply.github.com>
2026-01-12 23:08:30 +0100

    Feature: Replace colored box powerups with sprite-based rendering

--- src/World/ItemBarClient.cpp
+++ src/World/ItemBarClient.cpp
@@ -1,5 +1,6 @@
 #include "ItemBarClient.h"
 #include "ResourceManager.h"
+#include "Item/PowerupSpriteManager.h"
 
 ItemBarClient::ItemBarClient(PlayerState const &ownPlayer, sf::RenderWindow const &window)
 	: m_ownPlayer(ownPlayer), m_window(window), m_font(FontManager::inst().load("Font/LiberationSans-Regular.ttf"))
@@ -12,44 +13,6 @@ void ItemBarClient::draw(sf::RenderWindow &window) const
 
 	sf::Vector2f const windowSize(m_window.getSize());
 	sf::Vector2f const start = {windowSize.x / 2.f - hotbarWidth / 2.f, windowSize.y - slotSize - 20.f};
-
-	auto getColorForPowerup = [](PowerupType type) -> sf::Color {
-		switch(type)
-		{
-		case PowerupType::HEALTH_PACK:
-			return sf::Color::Green;
-		case PowerupType::SPEED_BOOST:
-			return sf::Color::Cyan;
-		case PowerupType::DAMAGE_BOOST:
-			return sf::Color::Red;
-		case PowerupType::SHIELD:
-			return sf::Color::Blue;
-		case PowerupType::RAPID_FIRE:
-			return sf::Color::Yellow;
-		case PowerupType::NONE:
-		default:
-			return sf::Color(60, 60, 60);
-		}
-	};
-
-	auto getNameForPowerup = [](PowerupType type) -> std::string {
-		switch(type)
-		{
-		case PowerupType::HEALTH_PACK:
-			return "HP";
-		case PowerupType::SPEED_BOOST:
-			return "SPD";
-		case PowerupType::DAMAGE_BOOST:
-			return "DMG";
-		case PowerupType::SHIELD:
-			return "SHD";
-		case PowerupType::RAPID_FIRE:
-			return "RPD";
-		case PowerupType::NONE:
-		default:
-			return "";
-		}
-	};
 
 	for(size_t i = 0; i < 9; ++i)
 	{
@@ -87,19 +50,11 @@ void ItemBarClient::draw(sf::RenderWindow &window) const
 		PowerupType itemType = inventory[i];
 		if(itemType != PowerupType::NONE)
 		{
-			sf::RectangleShape itemIcon(sf::Vector2f(slotSize - 10.f, slotSize - 10.f));
-			itemIcon.setPosition(sf::Vector2f(x + 5.f, start.y + 5.f));
-			itemIcon.setFillColor(getColorForPowerup(itemType));
-			window.draw(itemIcon);
+			constexpr float spriteScale = 0.5f;
 
-			sf::Text itemText(m_font);
-			itemText.setString(getNameForPowerup(itemType));
-			itemText.setCharacterSize(12);
-			itemText.setFillColor(sf::Color::White);
-			sf::FloatRect textBounds = itemText.getLocalBounds();
-			itemText.setPosition(sf::Vector2f(x + slotSize / 2.f - textBounds.size.x / 2.f,
-			                                  start.y + slotSize / 2.f - textBounds.size.y / 2.f - 3.f));
-			window.draw(itemText);
+			sf::Sprite itemSprite = PowerupSpriteManager::inst().getSpriteForType(itemType, spriteScale);
+			itemSprite.setPosition(sf::Vector2f(x + slotSize / 2.f, start.y + slotSize / 2.f));
+			window.draw(itemSprite);
 		}
 
 		sf::Text slotNumber(m_font);
